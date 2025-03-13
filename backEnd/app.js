5d28990ec32b53775443f9625322719640f7f568
Andre Nascimento <1914andre@gmail.com>
2025-03-13 11:03:15 -0300

    feat: adding try and catch in app

--- backEnd/app.js
+++ backEnd/app.js
@@ -4,5 +4,10 @@ const app = express()
 const PORT = 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  try {
+    console.log(`Server is running on port ${PORT}`)
+  }
+  catch (error) {
+    console.log(`Error: ${error.message}`)
+  }
 })

ebe043b8d229ec065def464bc15a21728e8c7e92
Andre Nascimento <1914andre@gmail.com>
2025-03-15 17:02:29 -0300

    feat: adding component title

---
+++ frontEnd/vite-project/src/components/title/title.jsx
@@ -0,0 +1,10 @@
+import "./title.css"
+
+export function Titles() {
+  return (
+    <div className="titles">
+       <h1>Elity Consultoria</h1>
+       <h3>faça sua consulta aqui!</h3>
+    </div>
+  );
+}

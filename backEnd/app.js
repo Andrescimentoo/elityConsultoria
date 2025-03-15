af23fe046cbee40c6e1a213c743d0ba80892f64b
Andre Nascimento <1914andre@gmail.com>
2025-03-15 17:10:24 -0300

    adding cors in project

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { routeMerchaintId } from './src/routes/merchaintId.routes.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
+
+app.use(cors({origin:('http://localhost:5173')}))
+
+app.use(routeMerchaintId)
 
 app.listen(PORT, () => {
   try {

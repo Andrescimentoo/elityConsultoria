1e7bd29391e052bb6179796a910dde8d50dd5169
Andre Nascimento <1914andre@gmail.com>
2025-05-14 23:01:01 -0300

    fix: altering sintax to component input

--- frontEnd/vite-project/src/components/input/input.jsx
+++ frontEnd/vite-project/src/components/input/input.jsx
@@ -11,14 +11,17 @@ function extrairMerchantId(url) {
 export function Input() {
   const [url, setUrl] = useState(""); // Para armazenar a URL
   const [merchantId, setMerchantId] = useState(""); // Para armazenar o merchantId extraído
-  const [cnpj, setCnpj] = useState(""); // Para armazenar o CNPJ da resposta
+  const [cnpj, setCnpj] = useState("");
+  const [name, setName] = useState ("") // Para armazenar o CNPJ da resposta
+  const [phoneIf,setPhoneIf] = useState ("")
+  const [adress,setAddress] = useState ("")
   // função de requsição para o back-end
   async function buscarDados() {
     try {
       // Extraímos o merchantId da URL
       const id = extrairMerchantId(url);
 
-      if (!id) throw new Error("Merchant ID inválido");
+      if (!id) throw new Error("Merchant ID inválido"); // se não for um merchaint id válido envia um erro
       setMerchantId(id); // Armazenamos o merchantId no estado
 
     const response = await fetch(`http://localhost:3000/myApiBackEnd?merchantId=${id}`, {
@@ -31,10 +34,14 @@ export function Input() {
     if (!response.ok) throw new Error("Erro ao buscar dados");
 
     const data = await response.json();
+    
 
-    if (!data.cnpj) throw new Error("CNPJ não encontrado na resposta");
+    if (!data.cnpj && data.name && data.phoneIf && data.address) throw new Error("CNPJ, nome e telefone de contato não encontrados na resposta");
 
-    setCnpj(data.cnpj);
+    setCnpj(data.cnpj)
+    setName(data.name)
+    setPhoneIf(data.phoneIf)
+    setAddress(data.address)
     } catch (error) {
       console.log("Erro na requisição:", error);
     }
@@ -49,17 +56,41 @@ export function Input() {
         onChange={(e) => setUrl(e.target.value)} // Atualiza a URL enquanto digita
       />
       <button onClick={buscarDados}>Buscar Dados</button>
-
+     
       {merchantId && (
-        <div>
-          <p>
-            <strong>Merchant ID:</strong> {merchantId}
-          </p>
-          <p>
-            <strong>CNPJ:</strong> {cnpj || "CNPJ não encontrado..."}
-          </p>
-        </div>
-      )}
+      <div className="interface_de_dados">
+        <ul>
+          <li>
+            <p><strong>CNPJ: </strong>{cnpj || "CNPJ não encontrado..."}</p>
+          </li>
+          <li>
+            <p><strong>Nome: </strong>{name || 'Nome não encontrado'}</p>
+          </li>
+          <li>
+            <p><strong>Telefone de contato: </strong>{phoneIf || 'Telefone de contato não encontrado'}</p>
+          </li>
+          
+          <li>
+            <p><strong>Cidade: </strong>{adress.city || 'Cidade não encontrada '}</p>
+          </li>
+          <li>
+            <p><strong>Estado: </strong>{adress.state || 'Estado não encontrado'}</p>
+          </li>
+          <li>
+            <p><strong>Distrito: </strong>{adress.district || 'Distrito não encontrado'}</p>
+          </li>
+          <li>
+            <p><strong>Rua: </strong>{adress.street || 'Rua não encontrada'}</p>
+          </li>
+          <li>
+            <p><strong>Número: </strong>{adress.number || 'Numero não encontrado'}</p>
+          </li>
+          <li>
+            <p><strong>CEP: </strong>{adress.zip || 'CEP não encontrado'}</p>
+          </li>
+        </ul>
+      </div>
+     )}
     </div>
   );
 }

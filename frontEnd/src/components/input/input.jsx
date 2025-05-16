1116eea1df2ea7c44456985c127173a341b6f0d8
Andre Nascimento <1914andre@gmail.com>
2025-05-16 11:14:18 -0300

    fix: try again new  sintax to Url

--- frontEnd/src/components/input/input.jsx
+++ frontEnd/src/components/input/input.jsx
@@ -26,7 +26,7 @@ export function Input() {
       setMerchantId(id); // Armazenamos o merchantId no estado
 
      
-      const response = await fetch(`https://elityconsultoria-1.onrender.com/myApiBackEnd=${id}`, {
+      const response = await fetch(`https://elityconsultoria-1.onrender.com/myApiBackEnd?merchantId=${id}`, {
         method: "GET",
         headers: {
           Accept: "application/json",

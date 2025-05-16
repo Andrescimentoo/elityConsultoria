df551ecb8336aa46882d497452e75624d7e97122
Andre Nascimento <1914andre@gmail.com>
2025-05-15 22:00:03 -0300

    fix: to correcting sintax of Url origin cors and adding middlewer

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,15 +5,13 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({origin:('https://frontendelityconsultoria.vercel.app/')}))
+app.use(cors({ origin: 'https://frontendelityconsultoria.vercel.app' }))
+app.use(express.json()) 
+
+app.get('/', (req, res) => res.send('Backend ativo!')) // rota de teste
 
 app.use(routeMerchaintId)
 
 app.listen(port, () => {
-  try {
-    console.log(`Server is running on port ${port}`)
-  }
-  catch (error) {
-    console.log(`Error: ${error.message}`)
-  }
+  console.log(`Server is running on port ${port}`)
 })

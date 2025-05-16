4b22f49a7421e83326f483a27caec5400b6d370c
Andre Nascimento <1914andre@gmail.com>
2025-05-15 22:52:29 -0300

    fix try new sintax in cors

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({ origin: 'https://frontendelityconsultoria.vercel.app' }))
+app.use(cors())
 app.use(express.json()) 
 
 app.get('/', (req, res) => res.send('Backend ativo!')) // rota de teste

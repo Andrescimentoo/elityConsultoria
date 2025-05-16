eefd2972b03c7b6145b1e3c59422200f1e02102a
Andre Nascimento <1914andre@gmail.com>
2025-05-15 21:27:31 -0300

    fix: changing origin to cors for new origin FrontEnd

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express()
 const PORT = 3000
 
-app.use(cors({origin:('http://localhost:5173')}))
+app.use(cors({origin:('https://frontendelityconsultoria.vercel.app/')}))
 
 app.use(routeMerchaintId)
 

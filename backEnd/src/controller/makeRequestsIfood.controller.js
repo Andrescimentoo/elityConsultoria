205a3e28a22a172d6b24bc8e1984c8f488073e25
Andre Nascimento <1914andre@gmail.com>
2025-05-16 22:28:46 -0300

    Applying desesconstruction in datas of request

--- backEnd/src/controller/makeRequestsIfood.controller.js
+++ backEnd/src/controller/makeRequestsIfood.controller.js
@@ -46,35 +46,19 @@ export const requestByMerchaintID = async (req, res) => {
       return res.status(response.status).json({ message: "Erro ao buscar dados da API externa" });
       
     }
-
-    const data = await response.json();
-       //data pra response
-       //const data = response.data.merchaintExtra
-       //const {name,documents,phoneIf,adress} = data
-       // data.
-    //if (!data.data?.merchantExtra?.documents?.CNPJ?.value || !data.data?.merchantExtra?.phoneIf) {
-     // return res.status(404).json({ message: "CNPJ,nome, numero de contato ou endereço não encontrados" });
-    //}
-    // return res.status(200).json({name,phoneIf,cnpj,adress})
+       const data = await response.json(); //guardando os dados em json, e dps escolhendo os dados que eu quero mandar pro front?
+       const datasToResponse = data.data.merchantExtra
+       const { name,phoneIf,documents,address} = datasToResponse
+       // DESCONSTRUÇÃO Serve pra extrair valores específicos de um objeto/array que você já tem.
+       
+        
     
+       if (!datasToResponse.phoneIf || !datasToResponse.address) {
+         return res.status(404).json({ message: "CNPJ,nome, numero de contato ou endereço não encontrados" });
+        }
+         return res.status(200).json({name,phoneIf,documents,address}) // documents contem o cnpj
     
-    
-    
-    
-    
-    return res.status(200).json({ // aqui também sinto que dá pra reconstruir
-      name: data.data.merchantExtra.name,
-      phoneIf: data.data.merchantExtra.phoneIf,
-      cnpj: data.data.merchantExtra.documents.CNPJ.value,
-      address:{
-         city: data.data.merchantExtra.address.city,
-         state: data.data.merchantExtra.address.state,
-         district: data.data.merchantExtra.address.district,
-         street: data.data.merchantExtra.address.streetName,
-         number: data.data.merchantExtra.address.streetNumber,
-         zip: data.data.merchantExtra.address.zipCode
-       } 
-      });
+       
   } catch (error) {
       console.error("Erro no processamento da requisição:", error.message);
     return res.status(500).json({ message: "Erro na requisição", error: error.message });

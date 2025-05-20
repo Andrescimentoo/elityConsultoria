66b437d11bf133fb76c6e708d9ae92d3dba53053
Andre Nascimento <1914andre@gmail.com>
2025-05-19 21:03:52 -0300

    feat: Created a new CORS configuration to allow multiple URLs as valid origins for requests.

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import { routeMerchaintId } from './src/routes/merchaintId.routes.js'
+import { allowedOrigins } from './cors.config.js'
 import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors('https://frontendelityconsultoria.vercel.app'))
+app.use(cors({
+  origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}))
 app.use(express.json()) 
 
 app.use(routeMerchaintId)
---
+++ backEnd/cors.config.js
@@ -0,0 +1,5 @@
+export const allowedOrigins = [
+  'http://localhost:5173',
+  'https://frontendelityconsultoria.vercel.app'
+  
+];

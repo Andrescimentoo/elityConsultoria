6b5d021d3a33b58cafd5034670acd76569b02149
Andre Nascimento <1914andre@gmail.com>
2025-05-19 21:54:51 -0300

    fix: adding new sintax in url

--- frontEnd/src/components/input/input.jsx
+++ frontEnd/src/components/input/input.jsx
@@ -1,54 +1,42 @@
 import React, { useState } from "react";
+import { extrairMerchantId } from "./extrairMarchaintId";
 
-// Função para extrair o merchantId da URL
-function extrairMerchantId(url) {
-  const regex = /(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/; // Regex para encontrar o UUID (merchantId)
-  const match = url.match(regex);
-  return match ? match[0] : null;
-}
-
-// estados que gerenciam o input
 export function Input() {
-  const [url, setUrl] = useState(""); // Para armazenar a URL
-  const [merchantId, setMerchantId] = useState(""); // Para armazenar o merchantId extraído
+  const [url, setUrl] = useState(""); 
+  const [merchantId, setMerchantId] = useState(""); 
   const [documents, setDocuments] = useState("");
-  const [name, setName] = useState(""); // Para armazenar o nome
+  const [name, setName] = useState(""); 
   const [phoneIf, setPhoneIf] = useState("");
   const [address, setAddress] = useState("");
 
-  // função de requisição para o back-end
+  
   async function buscarDados() {
     try {
-      // Extraímos o merchantId da URL
       const id = extrairMerchantId(url);
-
-      if (!id) throw new Error("Merchant ID inválido"); // se não for um merchant id válido envia um erro
-      setMerchantId(id); // Armazenamos o merchantId no estado
-
-     
-      const response = await fetch(`https://elityconsultoria.onrender.com/myApiBackEnd/merchantId=${id}`, {
+        if (!id) throw new Error("Merchant ID inválido"); // se não for um merchant id válido envia um erro
+        setMerchantId(id); // Armazenamos o merchantId no estado
+  
+      const response = await fetch(`https://elityconsultoria.onrender.com/myApiBackEnd?merchantId=${id}`, { 
         method: "GET",
         headers: {
           Accept: "application/json",
         },
       });
-
-      if (!response.ok) throw new Error("Erro ao buscar dados");
-
+         if (!response.ok) throw new Error("Erro ao buscar dados");
+       
       const data = await response.json();
 
-      if (!data.phoneIf && !data.address)
-        throw new Error("CNPJ, nome e telefone de contato não encontrados na resposta");
-
-      setDocuments(data.documents.CNPJ.value);
-      setName(data.name);
-      setPhoneIf(data.phoneIf);
-      setAddress(data.address);
+        if (!data.phoneIf && !data.address)
+          throw new Error("telefone de contato ou endereço não encontrados na resposta");
+        
+        setDocuments(data.documents.CNPJ.value);
+        setName(data.name);
+        setPhoneIf(data.phoneIf);
+        setAddress(data.address);
     } catch (error) {
-      console.log("Erro na requisição:", error);
+       console.log("Erro na requisição:", error);
     }
   }
-
   return (
     <div>
       <input

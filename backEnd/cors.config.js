2bd4e75d76ef7000aa2ad39d60a8379c4b238cc6
Andre Nascimento <1914andre@gmail.com>
2025-05-19 21:16:50 -0300

    feat: new URL of cnfig to CORS

--- backEnd/cors.config.js
+++ backEnd/cors.config.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
-  'http://localhost:5173',
-  'https://frontendelityconsultoria.vercel.app'
-  
+  'http://localhost:5173', 
+  'https://frontendelityconsultoria.vercel.app',
+  'prj_fAioILteUzSR1VdrWwbiyP2z2G3j' //https://frontendelityconsultoria.vercel.app/
+
 ];

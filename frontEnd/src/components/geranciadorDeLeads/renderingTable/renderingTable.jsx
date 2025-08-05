dd0e39cf444363f15885313e160f8e9a57d50838
Andre Nascimento <1914andre@gmail.com>
2025-08-05 19:50:47 -0300

    fix: test to structure

--- frontEnd/src/components/geranciadorDeLeads/renderingTable/renderingTable.jsx
+++ frontEnd/src/components/geranciadorDeLeads/renderingTable/renderingTable.jsx
@@ -7,15 +7,15 @@ export const RenderingTable = () => {
   return (
     <div>
       <ul>
-            <li><strong>CNPJ:</strong> {data?.documents?.CNPJ?.value || 'CNPJ não encontrado'}</li>
-            <li><strong>Nome:</strong> {data?.name || 'Nome não encontrado'}</li>
-            <li><strong>Telefone:</strong> {data?.phoneIf || 'Telefone não encontrado'}</li>
-            <li><strong>Cidade:</strong> {data.address?.city || 'Cidade não encontrada'}</li>
-            <li><strong>Estado:</strong> {data.address?.state || 'Estado não encontrado'}</li>
-            <li><strong>Bairro:</strong> {data.address?.district || 'Bairro não encontrado'}</li>
-            <li><strong>Rua:</strong> {data.address?.streetName || 'Rua não encontrada'}</li>
-            <li><strong>Número:</strong>{data.address?.streetNumber || 'Número não encontrado'}</li>
-            <li><strong>CEP:</strong> {data.address?.zipCode || 'CEP não encontrado'}</li>
+            <li><strong>CNPJ:</strong> {data.data?.documents?.CNPJ?.value || 'CNPJ não encontrado'}</li>
+            <li><strong>Nome:</strong> {data.data?.name || 'Nome não encontrado'}</li>
+            <li><strong>Telefone:</strong> {data.data?.phoneIf || 'Telefone não encontrado'}</li>
+            <li><strong>Cidade:</strong> {data.data?.address?.city || 'Cidade não encontrada'}</li>
+            <li><strong>Estado:</strong> {data.data?.address?.state || 'Estado não encontrado'}</li>
+            <li><strong>Bairro:</strong> {data.data?.address?.district || 'Bairro não encontrado'}</li>
+            <li><strong>Rua:</strong> {data.data?.address?.streetName || 'Rua não encontrada'}</li>
+            <li><strong>Número:</strong>{data.data?.address?.streetNumber || 'Número não encontrado'}</li>
+            <li><strong>CEP:</strong> {data.data?.address?.zipCode || 'CEP não encontrado'}</li>
       </ul>
     </div>
     

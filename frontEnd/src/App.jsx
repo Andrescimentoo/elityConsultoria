7a88e7ca5a2a1a6281507e8907e60f5d4734db1c
Andre Nascimento <1914andre@gmail.com>
2025-08-05 22:55:13 -0300

    fix: altering the app component

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import './App.css'
 import { GerarLeads } from './components/geranciadorDeLeads/gerarLeads/gerarLeads.jsx'
-import { Titles } from './components/geranciadorDeLeads/titleFazerConsulta/title.jsx'
+
 
 
 export function App() {
   
   return (
     <>
-      <section>
-        <header>
-          <Titles />
-        </header>
-  
-        <main>
+              
           <GerarLeads/>
-        </main>
        
-      </section>
+
+     
     </>
   )
 }

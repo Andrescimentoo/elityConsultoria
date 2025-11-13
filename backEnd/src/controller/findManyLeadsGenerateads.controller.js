11d9f5c73a09d8660cb3431a4c3eef28b49d717d
Andre Nascimento <1914andre@gmail.com>
2025-11-12 22:37:07 -0300

    feat: adding sintax and logic inside the controller to findmanyLeadsGenerateads.js

---
+++ backEnd/src/controller/findManyLeadsGenerateads.controller.js
@@ -0,0 +1,13 @@
+import { prisma } from "../prismaClient"
+
+export const findManyLeadsGenerateads = async (req,res) => {
+    try {
+        const consultedRestaurantsTables = await prisma.dataOfConsults.findMany()
+        res.status(200).send(consultedRestaurantsTables)
+    } catch (error) {
+        res.status(500).send({
+            error,
+            message: 'erro ao buscar todas as tabelas de restaurantes consultados'
+        })
+    }
+}

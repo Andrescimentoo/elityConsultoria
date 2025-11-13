2506d4163109f92c7d6010b389451fd301813119
Andre Nascimento <1914andre@gmail.com>
2025-11-12 23:01:48 -0300

    feat: altering name to endpoints and variable to route in routesTodatasOfLeads

---
+++ backEnd/src/routes/routesToDatasOfLeads.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { saveLeads } from "../controller/saveLeads.controller.js";
+import { findManyLeadsGenerateads } from "../controller/findManyLeadsGenerateads.controller.js";
+
+export const routesToDatasOfLeads = Router()
+
+routesToDatasOfLeads.get('/findManyleads',findManyLeadsGenerateads) // essa rota avisa o controller findManyLeads para ele pegar todas as tabelas dos leads gerados e enviar para o front-end  
+routesToDatasOfLeads.post('/saveLeads',saveLeads) // essa rota avisa o controller storeMoreLeads pra ele guardar mais um lead gerado na base de dados
+
+
+
--- backEnd/src/routes/routesTodatasOfConsultes.routes.js
+++
@@ -1,8 +0,0 @@
-import { findManyRestaurants } from "../controller/findManyRestaurants.controller";
-import router from "express/lib/router";
-import { storeNewRestaurant } from "../controller/storeNewRestaurant.controller";
-
-export const routesToDatasOfConsultes = router()
-
-routesToDatasOfConsultes.get('/conceitualNameGet',findManyRestaurants)
-routesToDatasOfConsultes.post('/conceeitualnamePost',storeNewRestaurant)

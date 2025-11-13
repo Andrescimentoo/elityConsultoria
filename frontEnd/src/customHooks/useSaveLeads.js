e8462cf3b6ad7cd8d8a77882fcac7cba2c490e68
Andre Nascimento <1914andre@gmail.com>
2025-11-12 23:06:11 -0300

    feat: Adding custom hook useSaveLeads.js

---
+++ frontEnd/src/customHooks/useSaveLeads.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useDados } from './useDados'
+
+export const UseSaveLeads = () => {
+  const {name,documents,address,phoneIf} = useDados() // passando por props os dados do Usedados 
+
+  const requestSaveLeads = async () => {
+    try {
+      const reqSalvarLeads = await fetch('/endpointSaveLeads', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          documents,
+          address,
+          phoneIf
+        })
+      })
+
+      if (!reqSalvarLeads.ok) {
+        throw new Error('Erro ao salvar no servidor')
+      }
+      
+    } catch (error) {
+     console.log('Erro ao salvar: ' + error.message)
+    }
+  }
+
+  return (
+   requestSaveLeads
+  )
+}

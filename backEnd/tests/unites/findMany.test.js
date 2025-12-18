b7041fffd42f3386a301c2783424c954f071a2bd
Andre Nascimento <1914andre@gmail.com>
2025-12-17 21:16:23 -0300

    feat: adding prototpy to test In back-end

---
+++ backEnd/tests/unites/findMany.test.js
@@ -0,0 +1,5 @@
+
+test('testar se todos os dados são enviados como resposta ', () => {
+  expect('espera-se que aconteça.....').toBe('e aqui a consequencia ou o resultado do que acontecer edentro do expect') 
+})
+

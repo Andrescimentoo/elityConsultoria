729dd395c64196ec64ccce9b89eb341a44783484
Andre Nascimento <1914andre@gmail.com>
2025-12-17 21:21:20 -0300

    feat: adding structure of rendering datas

--- frontEnd/src/components/exibirHistoricoDeLeads/exibirHistoricoDeLeads.jsx
+++ frontEnd/src/components/exibirHistoricoDeLeads/exibirHistoricoDeLeads.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 
 export const ExibirHistoricoDeLeads = () => {
+  const [datas,setDatas] = useState('') // esse é o estado
+  
   return (
-    <div>
-      <h1>Olá você conseguiu usar o react Router</h1>
+    <>
+       <div>
+        Exibir Histórico de leads 
+       </div> 
+
+      <div>
+      <ul>
+        <li><strong>Nome:</strong> {data.name || 'Nome não encontrado'}</li>
+        <li><strong>Telefone:</strong> {data.phoneIf || 'Telefone não encontrado'}</li>
+        <li><strong>Cidade:</strong> {data.address?.city || 'Cidade não encontrada'}</li>
+        <li><strong>Estado:</strong> {data.address?.state || 'Estado não encontrado'}</li>
+        <li><strong>Bairro:</strong> {data.address?.district || 'Bairro não encontrado'}</li>
+        <li><strong>Rua:</strong> {data.address?.streetName || 'Rua não encontrada'}</li>
+        <li><strong>Número:</strong> {data.address?.streetNumber || 'Número não encontrado'}</li>
+        <li><strong>CEP:</strong> {data.address?.zipCode || 'CEP não encontrado'}</li>
+      </ul>
     </div>
+
+       <div>
+         <button onClick={useShowLeadshistory}>Exibir Histórico de Leads</button>
+       </div>
+    </>
+   
   )
 }
+
+
+
+
+// vou ter q criar um webHook separado também por que além da chamada vai ter a renderização dos dados através de estados.
+
 
 

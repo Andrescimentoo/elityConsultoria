0b12a243a5379de7566377c1648f850a2784605e
Andre Nascimento <1914andre@gmail.com>
2026-01-01 14:04:29 -0300

    fix(routes): fix controller import by adding .js extension

--- backEnd/src/routes/routeRequestIfoodAPI.routes.js
+++ backEnd/src/routes/routeRequestIfoodAPI.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { makeRequestIfoodAPI } from "../controller/makeRequestIfood.controller";
+import { makeRequestIfoodAPI } from "../controller/makeRequestIfood.controller.js";
 
 export const routeRequestIfoodAPI = Router()
 
-routeRequestIfoodAPI.get("/requestToIfoodAPI",makeRequestIfoodAPI)
+routeRequestIfoodAPI.get("/requestToIfoodAPI", makeRequestIfoodAPI)
 
 // essa rota avisa o controller para ele fazer a requisição para a api do ifood pegar os dados do lead e enviar pro front-end  

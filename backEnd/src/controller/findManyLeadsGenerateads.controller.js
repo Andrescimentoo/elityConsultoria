f6ed2bcc89834eba66d273df761997278f06a7f9
Andre Nascimento <1914andre@gmail.com>
2026-01-01 14:04:23 -0300

    fix(controller): fix prisma model reference and import extension

--- backEnd/src/controller/findManyLeadsGenerateads.controller.js
+++ backEnd/src/controller/findManyLeadsGenerateads.controller.js
@@ -1,13 +1,14 @@
-import { prisma } from "../prismaClient"
+import { prisma } from "../prismaClient.js"
+
 
 export const findManyLeadsGenerateads = async (req,res) => {
     try {
-        const consultedRestaurantsTables = await prisma.dataOfConsults.findMany()
+        const consultedRestaurantsTables = await prisma.datasOfConsults.findMany()
         res.status(200).send(consultedRestaurantsTables)
     } catch (error) {
         res.status(500).send({
             error,
-            message: 'erro ao buscar todas as tabelas de restaurantes consultados'
-        })
+            message:'erro ao buscar todas as tabelas de restaurantes consultados'
+        }) 
     }
 }

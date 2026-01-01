b8e56afe58aa25cd114b03d19ae1d9b35108d0f7
Andre Nascimento <1914andre@gmail.com>
2026-01-01 14:08:53 -0300

    feat(test): add unit test for findMany services

--- backEnd/tests/unites/findMany.test.js
+++
@@ -1,5 +0,0 @@
-
-test('testar se todos os dados são enviados como resposta ', () => {
-  expect('espera-se que aconteça.....').toBe('e aqui a consequencia ou o resultado do que acontecer edentro do expect') 
-})
-

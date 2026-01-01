cf9f81d8e0888414c247ecb112c372b590e0f971
Andre Nascimento <1914andre@gmail.com>
2026-01-01 14:04:54 -0300

    fix(hooks): remove unused error variables and cleanup imports

--- frontEnd/src/customHooks/useDados.js
+++ frontEnd/src/customHooks/useDados.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { extrairMerchantId } from '../components/geranciadorDeLeads/inputCapturaMerchantId/extrairMarchaintId.js';
- 
 
-export function useDados() { 
+
+export function useDados() {
   const [url, setUrl] = useState('');
   const [data,setData] = useState('')
-  
+
   const buscarDados = async () => { // mudar para buscar dados do Ifood
     try {
-        const id = extrairMerchantId(url);
+      const id = extrairMerchantId(url);
         const response = await fetch(`https://elityconsultoria.onrender.com/gerarLeadsViaMerchantId?merchantId=${id}`, {
         method: 'GET',
         headers: { Accept: 'application/json' }
@@ -16,13 +16,13 @@ export function useDados() {
       if (!response.ok) throw new Error('Erro ao buscar dados');
 
       const data = await response.json();
-      
+
       setData(data)
       setUrl(id)
 
       if (!data.phoneIf && !data.address)
-        throw new Error('telefone de contato ou endereço não encontrados'); 
-    
+        throw new Error('telefone de contato ou endereço não encontrados');
+
     } catch (error) {
       console.log('Erro na requisição:', error);
     }
